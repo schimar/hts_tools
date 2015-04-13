b0a174412af0f122472dbf9f3770b33997f0d197
schimar <martin.schilling@usu.edu>
2015-04-13 09:29:47 -0600

    added consensus_clusters-hist

--- cluste.r
+++ cluste.r
@@ -12,7 +12,7 @@ for (i in files){
 
 
     runinf[i] <- readLines(i)
-}
+
 
      
 for(i in 1:6) { #-- Create objects  'r.1', 'r.2', ... 'r.6' --
@@ -26,13 +26,19 @@ for(i in 1:6) { #-- Create objects  'r.1', 'r.2', ... 'r.6' --
 
 
 
+#############
 
+setwd("/home/mschilling/Desktop/gbs15/ind/clustered_vsearch/sub_centroids/2014/")
 
+clust_cons <- read.csv("lasio_consensus_clusters.csv", header= F)
+# or better: 
+clust <- read.csv("lasio_paralogs2_clusters.csv", header= F)
 
+##
+hist(clust$V2, ylim= c(0, 500))
+hist(clust$V3, ylim= c(0, 500))
 
-
-
-
-
+mrgd <- merge(clust, clust_cons, by="V1") 
+table(mrgd$V2.x == mrgd$V2.y) # just checking if the two counts are actually the same
 
 
