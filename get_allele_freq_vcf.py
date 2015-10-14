2d7c46e2db9bf5e3e270007e6ecd9b8b9d67b394
schimar <schimar@gmail.com>
2015-10-14 13:21:52 -0600

    and another

--- get_allele_freq_vcf.py
+++ get_allele_freq_vcf.py
@@ -35,6 +35,10 @@ with open(sys.argv[1], 'rb') as file:
                 aa = 10**(aa/-10)
                 if rr > ra and rr > aa:
                     print rr, line_list[4], ra, aa, gt_lklhd
+                elif ra > rr and ra > aa:
+                    print ra, line_list[4], rr, aa, gt_lklhd
+                else: 
+                    print aa, line_list[4], rr, aa, gt_lklhd
     print n_snv
             #type = line_list[2]
             #gene = re.findall('[A-Z][a-z]+\.[0-9]+s[0-9]+\.v[0-9]+\.[0-9]+', line_list[8])
