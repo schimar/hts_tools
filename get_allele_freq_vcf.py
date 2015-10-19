81121607dce0bc16abf43f476218d1b1d81da196
schimar <schimar@gmail.com>
2015-10-19 16:15:38 -0600

    yet another

--- get_allele_freq_vcf.py
+++ get_allele_freq_vcf.py
@@ -21,27 +21,31 @@ with open(sys.argv[1], 'rb') as file:
             header = line.split('\t')
             ind_ids = header[9:len(header)]
             ind_ids[62] = ind_ids[62].split('\n')[0]
+            print ind_ids
         else:
             line_list = line.split('\t')
             if len(line_list[4]) > 1:
                 continue
             else:
+                geno_likely = dict()
                 n_snv += 1
                 id = line_list[1]
                 af = re.findall('AF1=[0.0-9.0]+', line_list[7])
                 af = float(af[0].split('=')[1])
                 rf = 1 - af
                 for j, ind in enumerate(ind_ids):
-                    print ind, line_list[j+9]
-                    
-                    #gt_lklhd = line_list[9].split(':')
-                #rr, ra, aa = map(int, gt_lklhd[1].split(','))
-                #cov = int(gt_lklhd[2])
-                #prob_F = 10**(int(gt_lklhd[3])/-10)
-                #               rr = 10**(rr/-10)
+                    #print ind, line_list[j+9]
+                    gt_lklhd = line_list[j+9].split(':')
+                    rr, ra, aa = map(int, gt_lklhd[1].split(','))
+                    cov = int(gt_lklhd[2])
+                    prob_F = 10**(int(gt_lklhd[3])/-10)
+                    ind_line = str(rr + ' ' + ra + ' ' + aa + ' ' + cov + ' ' + prob_F)
+                    geno_likely[ind] = ind_line
+                #rr = 10**(rr/-10)
                 #ra = 10**(ra/-10)
                 #aa = 10**(aa/-10)
                 #print id, rf, af, rr, ra, aa, cov, prob_F
+            print geno_likely.values()
 
 #    print n_snv
     file.close()
