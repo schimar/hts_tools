a2bbb8c8d28da78827492ad6bc81e10142a3f2ac
schimar <martin.schilling@usu.edu>
2015-10-31 14:56:35 -0600

    both files read in, created dict, and loop over values for each key in dict

--- get_codingSeq_bool_perSNV.py
+++ get_codingSeq_bool_perSNV.py
@@ -9,24 +9,35 @@
 import sys
 import re
 
+with open(sys.argv[2], 'rb') as codS_file:
+    scaf = dict()
+    for line in codS_file:
+        line_list = line.split('\t')
+        cscaf = line_list[0].split(':')[1].split('_')[1]
+        start = line_list[1]
+        stop = line_list[2].split('\n')[0]
+        if not cscaf in scaf:
+            scaf[cscaf] = [str(start + ' ' + stop)]
+        else:
+            scaf[cscaf].append((str(start + ' ' + stop)))
+#print len(scaf.keys()), len(scaf.values())
 
-
- = open(sys.argv[2], 'a')
 with open(sys.argv[1], 'rb') as file:
     for line in file:
         line_list = line.split(' ')
         scaffold, snv_id = line_list[0].split(':')
         naive_freq = line_list[1]
         final_freq = line_list[2]
-
+        if scaffold in scaf:
+            for key in scaf[scaffold]:
+                print key.split(' ')
+        else:
+            continue
+            
+            #print scaffold, len(scaf[scaffold])
         #print scaffold, snv_id, naive_freq, final_freq
 
-
-
-      
-      
-      
-      
-      
+   
     file.close()
+    codS_file.close()
         
