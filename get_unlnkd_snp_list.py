3a257342bd488b16bd47bbdb39cc7e1f3e1c4121
schimar <martin.schilling@usu.edu>
2015-11-14 12:28:07 -0700

    done, although it doesn't pop out a list with the SNPs - not needed (yet?)

--- get_unlnkd_snp_list.py
+++ get_unlnkd_snp_list.py
@@ -1,40 +1,45 @@
 #! /usr/bin/python
 
 
-# This script reads through a genotype likelihood file for a given population,
-# as obtained by Zach Gompert's script subvcf2gl.pl (which separates populations
-# from a vcf file (from bcftools)) and a text file containing the coding
-# sequences with their start/stop positions. If a SNV lies in a coding sequence,
-# then it will print the respective scaffold:SNV_pos combination.
-# (see also sites.google.com/site/evolutionarygenomicsfall15/assignment-6)
+# This script reads through a file containing the scaffold number and snp_id
+# (space-separated) and a second file, containing the allele frequencies in the
+# specified format for bayenv2 (see
+# http://www.eve.ucdavis.edu/gmcoop/Software/Bayenv/Bayenv.html for more info).
+# The output contains allele frequencies for the first SNP of each scaffold. 
 
-# Usage: ./ngs_tools/get_codingSeq_bool_perSNV.py <pop_gl_file> <codingSeq_file>
-# > outfile
+# (see also sites.google.com/site/evolutionarygenomicsfall15/assignment-7)
+# Usage: ./ngs_tools/get_unlnkd_snp_list.py <scaffold-snp-file> <file> > outfile
+
 
 import sys
 import re
 
-with open(sys.argv[2], 'rb') as scaf_file:
+with open(sys.argv[1], 'rb') as scaf_file:
     #scaf = dict()
-    for line in codS_file:
+    n = int()
+    snp_id_list = list()
+    prior_scaf = str()
+    for i, line in enumerate(scaf_file):
         scaffold, snp = line.split(' ')
-            print scaffold, snp
-#with open(sys.argv[1], 'rb') as file:
-#    for line in file:
-#        line_list = line.split(' ')
-#        scaffold, snv_id = line_list[0].split(':')
-#        naive_freq = line_list[1]
-#        final_freq = line_list[2]
-#        if scaffold in scaf:
-#            for key in scaf[scaffold]:
-#                start, stop = key.split(' ')
-#                if snv_id > start and snv_id < stop:
-#                    print line_list[0]
-#                else:
-#                    continue
-#        else:
-#            continue
-            
-#    file.close()
-    .close()
+        if scaffold != prior_scaf:
+            n += 1
+            snp_line_a1 = i*2
+            snp_id_list.append(snp_line_a1)
+            snp_id_list.append(snp_line_a1 + 1)
+        else: 
+            continue 
+        prior_scaf = scaffold
+
+# snp_id_double = [x * 2 for x in snp_id_list]
+#print snp_id_list
+#print snp_id_double
+
+
+with open(sys.argv[2], 'rb') as allele_file:
+    for i, line in enumerate(allele_file):
+        if i in snp_id_list:
+            print line.split('\n')[0]
+
+    allele_file.close()
+    scaf_file.close()
         
