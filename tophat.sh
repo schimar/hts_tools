b7d098aada9054b0a09139eb6fd9009655fe3474
schimar <schimar@gmail.com>
2015-11-26 12:35:24 -0700

    ...

--- tophat.sh
+++ tophat.sh
@@ -3,8 +3,8 @@
 
 for i in *.fastq; do
     id=$(echo $i | cut -f1 -d.)
-    echo $id
-    tophat -p 4 -N 3 -o /home/mschilling/Desktop/gbs15/mstr/tophat_out/ Bstricta $i
+    echo $id, $i
+    #tophat -p 4 -N 3 -o /home/mschilling/Desktop/gbs15/mstr/tophat_out/ Bstricta $i
     
 done
 
