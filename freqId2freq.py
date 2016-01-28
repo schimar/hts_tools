26c1fc6b6c3c58193006d0c94b9a3563dc14bd64
schimar <schimar@gmail.com>
2016-01-28 16:39:25 -0700

    minor change

--- freqId2freq.py
+++ freqId2freq.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
-# 
-# This script reads 
+#
+# This script reads
 
 # Usage: ./
 
@@ -12,8 +12,9 @@ import re
 #newfile = open(sys.argv[2], 'a')
 with open(sys.argv[1], 'rb') as file:
         for i, line in enumerate(file):
-            freq = line.split(' ')[1]
-            print freq.split('\n')[0]
+            freq = line.split('\n')[0]
+            line_list = freq.split(' ')
+            print line_list[2]  #, line_list[2] #freq.split('\n')[0]
 	file.close()
 
 
