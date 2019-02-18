2bb2238d36d52fd2ad2deca12f2d962644aef204
schimar <schimar.com>
2019-02-18 08:04:08 +0100

    renamed sh

--- bwa_mem.sh
+++ bwa_mem.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # cd ~/Desktop/boechera/lasio/
 
-for i in ~/Desktop/boechera/lasio/vsearch/run/*.fastq; do
+for i in *.fastq; do
     file=$(echo $i | cut -f9 -d/)
     id=$(echo $file | cut -f1 -d.)
     #id=$(echo $id | cut -f7 -d/)
@@ -11,7 +11,7 @@ for i in ~/Desktop/boechera/lasio/vsearch/run/*.fastq; do
 
     #~/bwa/bwa mem -t 10 -w 50 -k 20 -a -C -R "@RG\tID:$id\tLB:$id\tSM:$id\tPL:ILLUMINA" ~/Desktop/stricta/stricta_assembly/v1.2/assembly/Bstricta_278_v1.fa $i > bwaMEMout/${id}.sam
 	#~/bwa/bwa mem -t 10 -w 50 -k 20 -a -C -R "@RG\tID:$id\tLB:$id\tSM:$id\tPL:ILLUMINA" l105_88strictaMEM2Cns76_5.fasta $i > out/AllRds/${id}.sam
-	~/bwa/bwa mem -t 10 -w 50 -k 20 -a -C -R "@RG\tID:$id\tLB:$id\tSM:$id\tPL:ILLUMINA" l105_88strictaMEM2Cons76_5hdr_clnd.fasta $i > out/AllRds/${id}.sam
+	~/bwa/bwa mem -t 10 -w 50 -k 20 -a -C -R "@RG\tID:$id\tLB:$id\tSM:$id\tPL:ILLUMINA" l105_88strictaMEM2Cons76_5hdr_clnd.fasta $i > ${id}.sam
 
 done
 
